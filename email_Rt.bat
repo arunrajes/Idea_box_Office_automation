23f3df2e9f95128b893e95b8f855379a2001184f
Arun Rajesh Balakrishnan <arunrajes@gmail.com>
2020-02-04 06:43:24 +0530

    Add files via upload
    Windows Batch file for automatic execution of R Script

---
+++ email_Rt.bat
@@ -0,0 +1,4 @@
+@ECHO off
+ECHO Congratulations! Your first batch file executed successfully
+"C:\Users\gssaruba\Documents\R\R-3.6.1\bin\R.exe" CMD BATCH C:\Users\gssaruba\Documents\R_Scripts\emailing_R.R
+PAUSE
